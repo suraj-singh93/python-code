0357ad5d224e8d9060b73231f7007fdbe76039f3
Suraj Kumar Singh <suraj_singh93@hotmail.com>
2018-08-16 19:58:44 +0530

    Update lmba.py
    comment added

--- lambda-func/lmba.py
+++ lambda-func/lmba.py
@@ -1,15 +1,16 @@
+## to print all number in a list divided by 7 but not by 5
+
 
 def func(x):
     if  x % 5  and not x % 7:
         return x
 
-
-a = list(filter(lambda x: int(x % 5) and not x % 7, [10,15,14,7,16]))
-
-print(a)
-
 a = [1,0,8,7,14]
+## solution first using filter
+b = list(filter(lambda x: int(x % 5) and not x % 7, a))
+print(b)
 
+### Solution using list comparhension
 print([x for x in a if x % 5 and not x % 7 ])
 
 
